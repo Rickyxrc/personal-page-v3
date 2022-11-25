f54ffd81ee1a6873271043ff718e5ac9445a2559
Rickyxrc <rickyxrc@outlook.com>
2022-11-25 23:31:26 +0800

    添加底栏

--- src/App.vue
+++ src/App.vue
@@ -24,12 +24,19 @@ Rickyxrc.dream = "To make this world better";
         </pre>
       </div>
     </div>
+    <div class="container-2">
+      <div class="mid">
+        <a href="https://blog.rickyxrc.top">个人博客</a>&nbsp;
+        |&nbsp;<a href="https://github.com/Rickyxrc">Github</a>&nbsp;
+        |&nbsp;<a href="https://luogu.com.cn/387961/">Luogu</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-pre{
-  margin:0;
+pre {
+  margin: 0;
 }
 
 .base-container {
@@ -43,11 +50,29 @@ pre{
   left: 10vw;
   top: 15vh;
   width: 80vw;
-  height: 80vh;
+  height: 60vh;
   background: #fffefe;
   border-radius: 4px;
-  box-shadow: 0px 0px 48px 0px rgba(21, 235, 199, 0.27);
+  box-shadow: 0px 0px 48px 0px rgba(21, 235, 199, 0.13);
 }
+
+.container-2 {
+  position: absolute;
+  left: 10vw;
+  top: 80vh;
+  width: 80vw;
+  height: 10vh;
+  background: #fffefe;
+  border-radius: 4px;
+  box-shadow: 0px 0px 48px 0px rgba(21, 235, 199, 0.15);
+}
+
+.container-2>.mid {
+  align-self: center;
+}
+
+.container-2>.mid>a {color: #738de2;}
+
 .container:before {
   content: url("/head.png");
   position: absolute;
@@ -61,13 +86,12 @@ pre{
 .subcontainer {
   width: 80vw;
   height: 16.66vh;
-  background: linear-gradient(
-    155.03deg,
-    rgba(34, 225, 255, 1) 0%,
-    rgba(29, 143, 224, 1) 72.24%,
-    rgba(97, 93, 176, 1) 100%
-  );
+  background: linear-gradient(155.03deg,
+      rgba(34, 225, 255, 1) 0%,
+      rgba(29, 143, 224, 1) 72.24%,
+      rgba(97, 93, 176, 1) 100%);
 }
+
 .subcontainer-2 {
   width: 80vw;
   height: 63.33vh;
@@ -76,8 +100,8 @@ pre{
 .name {
   position: absolute;
   right: 8vw;
-  top: 6rem;
-  font-size:3rem;
-  color:#fff;
+  top: 5vh;
+  font-size: 3rem;
+  color: #fff;
 }
 </style>
